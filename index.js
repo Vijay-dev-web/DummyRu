8aeb791c7aee9c17ebb90c82d9ec80ee069e4a7f
Jayavictor <116828849+Jayavictor@users.noreply.github.com>
2022-10-27 21:40:32 +0530

    Update index.js (#13)

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use("/", (req, res) > {
+  res.send({
+    status: 200,
+    message: "Success"
+  })
+})
+
 app.listen(port, () => {
   console.log("Server active at port", port);
 )
