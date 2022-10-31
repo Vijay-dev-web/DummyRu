b6e9333866522528a8081ababfe7c85a14160ad7
ShinySajal <117103240+ShinySajal@users.noreply.github.com>
2022-10-31 21:34:22 +0530

    Added new route (#19)

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.get('/home', (req, res) => {
   })
 })
 
+app.get('/hacktoberfest', (req, res) => {
+  res.send({
+      status: 200,
+      message: "My First contribution to hacktoberfest!"
+  })
+})
+
 app.listen(port, () => {
   console.log("Server active at port", port);
 )
